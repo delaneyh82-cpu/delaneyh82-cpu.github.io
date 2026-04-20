8552e3756077bdd87072a3eb055e11dcd8e0ad3d
Delaney Hutton <delaneyh82@gmail.com>
2026-04-20 13:43:09 -0400

    mondayyy

---
+++ LAB12/JS/script.js
@@ -0,0 +1,14 @@
+console.log("Javascript is connected!");
+
+// Open the overlay menu
+
+function openOverlay(){
+    document.getElementById("overlay-menu").style.width = "100vw"
+}
+
+
+
+// Close the overlay menu
+function closeOverlay(){
+    document.getElementById("overlay-menu").style.width = "0"
+}
